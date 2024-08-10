661cbb578f44ffab75ffd0d6aeaff40a3eaf26c2
felipepinho <lfelipepinho@gmail.com>
2024-08-10 10:49:06 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/domain/models/dtos/CategoriaResponseDto.java
@@ -0,0 +1,9 @@
+package br.com.cotiinformatica.domain.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class CategoriaResponseDto {
+	private Integer id;
+	private String nome;
+}
---
+++ src/main/java/br/com/cotiinformatica/domain/models/enums/TipoConta.java
@@ -0,0 +1,6 @@
+package br.com.cotiinformatica.domain.models.enums;
+
+public enum TipoConta {
+	RECEBER,
+	PAGAR
+}
